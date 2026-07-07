d5a6f4b1875ea4e94c06a864815759068688c90a
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-07-07 21:33:24 +0530

    Update SettingsActivity.kt

--- app/src/main/java/com/example/cipherkeyboard/SettingsActivity.kt
+++ app/src/main/java/com/example/cipherkeyboard/SettingsActivity.kt
@@ -1,7 +1,26 @@
-<?xml version="1.0" encoding="utf-8"?>
-<input-method xmlns:android="http://schemas.android.com/apk/res/android">
-    <subtype
-        android:label="Cipher Keyboard"
-        android:imeSubtypeLocale="en_US"
-        android:imeSubtypeMode="keyboard" />
-</input-method>
+package com.example.cipherkeyboard
+
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+
+        val editText = findViewById<EditText>(R.id.key_input)
+        val saveButton = findViewById<Button>(R.id.save_button)
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        editText.setText(prefs.getString("cipher_key", "KEY"))
+
+        saveButton.setOnClickListener {
+            prefs.edit().putString("cipher_key", editText.text.toString()).apply()
+            Toast.makeText(this, "Key saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
