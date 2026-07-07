3ec56d63fc47f08de4d0a75ef98b6e0b1bd0723a
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-07-07 22:26:51 +0530

    Update CipherInputMethodService.kt

--- app/src/main/java/com/example/cipherkeyboard/CipherInputMethodService.kt
+++ app/src/main/java/com/example/cipherkeyboard/CipherInputMethodService.kt
@@ -1,2 +1,34 @@
-include ':app'
-rootProject.name = "CipherKeyboard"
+package com.example.cipherkeyboard
+
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
+import android.view.View
+
+class CipherInputMethodService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+
+    override fun onCreateInputView(): View {
+        val kv = KeyboardView(this, null)
+        val k = Keyboard(this, R.xml.qwerty)
+        kv.keyboard = k
+        kv.setOnKeyboardActionListener(this)
+        return kv
+    }
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+        val ic = currentInputConnection ?: return
+        when (primaryCode) {
+            -5 -> ic.deleteSurroundingText(1, 0) // Backspace බටන් එක වැඩ කරන්න
+            32 -> ic.commitText(" ", 1)        // Space බටන් එක වැඩ කරන්න
+            else -> ic.commitText(primaryCode.toChar().toString(), 1)
+        }
+    }
+
+    override fun onPress(primaryCode: Int) {}
+    override fun onRelease(primaryCode: Int) {}
+    override fun onText(text: CharSequence?) {}
+    override fun swipeLeft() {}
+    override fun swipeRight() {}
+    override fun swipeDown() {}
+    override fun swipeUp() {}
+}
